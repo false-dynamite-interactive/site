ddf9b0917707ec92489aea546c787caa434e506e
False Dynamite Interactive <149907031+false-dynamite-interactive@users.noreply.github.com>
2024-01-30 16:15:03 -0600

    Added da data shit

---
+++ data/404redirect.js
@@ -0,0 +1,3 @@
+location.replace("404.html"); 
+
+// This is a basic script that will be applied to any page that is either under construction or just doesn't fucking exist.
---
+++ data/crew.js
@@ -0,0 +1,39 @@
+var num = 1;
+
+function NextPerson()
+{
+    num++;
+    console.log(num);
+    
+    if (num === 2)
+    {
+        document.getElementById("memPfp").src = "data/devpfp/fdi_nick.png";
+        document.getElementById("memName").innerHTML = '<p>Nick Armes</p>';
+        document.getElementById("memPos").innerHTML = '<p>Co-Founder, COO, Developer</p>';
+    }
+    else if (num === 3)
+    {
+        document.getElementById("memPfp").src = "data/devpfp/fdi_das.png";
+        document.getElementById("memName").innerHTML = '<p>Das Baby (D.V.D.)</p>';
+        document.getElementById("memPos").innerHTML = '<p>COO, Dev, Writer</p>';
+    }
+    else if (num === 4)
+    {
+        document.getElementById("memPfp").src = "data/devpfp/fdi_nathan.png";
+        document.getElementById("memName").innerHTML = '<p>Nathan E.</p>';
+        document.getElementById("memPos").innerHTML = '<p>Manager, Developer, Artist</p>';        
+    }
+    else if (num === 5)
+    {
+        document.getElementById("memPfp").src = "data/devpfp/fdi_alex.png";
+        document.getElementById("memName").innerHTML = '<p>Alex Fallon</p>';
+        document.getElementById("memPos").innerHTML = '<p>Developer</p>';        
+    }
+    else if (num >= 6)
+    {
+        num = 1;
+        document.getElementById("memPfp").src = "data/devpfp/fdi_john.png";
+        document.getElementById("memName").innerHTML = '<p>John Fiore</p>';
+        document.getElementById("memPos").innerHTML = '<p>Founder, CEO, Director, Developer</p>';
+    }
+}
